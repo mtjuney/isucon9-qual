ef2eda8e344228026e0182d33a1384dce2e549d5
mtjuney <mtjuney@gmail.com>
2019-09-07 16:00:55 +0900

    campaign 4

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -542,7 +542,7 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 
 	res := resInitialize{
 		// キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-		Campaign: 4,
+		Campaign: 2,
 		// 実装言語を返す
 		Language: "Go",
 	}
